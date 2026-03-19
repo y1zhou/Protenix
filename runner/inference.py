c5f95477039f88ba9fb0008d372d2a5e98e2b139
y1zhou <zhou.zy.yi@gmail.com>
2026-03-19 13:38:27 +0800

    fix(fast_layernorm): revert to fast_layernorm by default now that it's working on Blackwell

--- runner/inference.py
+++ runner/inference.py
@@ -118,7 +118,7 @@ class InferenceRunner(object):
                 "is first called."
             )
 
-        use_fastlayernorm = os.getenv("LAYERNORM_TYPE", "torch")
+        use_fastlayernorm = os.getenv("LAYERNORM_TYPE", "fast_layernorm")
         if use_fastlayernorm == "fast_layernorm":
             logging.info(
                 "Kernels will be compiled when fast_layernorm is first called."
